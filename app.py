325181ecb045b38c1448be6723efaa131c6c27b7
sky <skybride@hotmail.com>
2023-05-14 18:24:23 -0400

    Added to do list with crud operations

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks, enumerate=enumerate)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+    tasks.append(task)
+    return redirect('/')
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    task_index = int(request.form[task_index])
+    del tasks[task_index]
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
